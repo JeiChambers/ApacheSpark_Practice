11b5f3d07e3d247e8769f1190888d52959e795a0
Jei Chambers <jeilee88@outlook.com>
2019-05-16 23:35:16 -0500

    Late Save

--- SparkScalaCourse/src/com/jeitheronin/spark/FriendsByName.scala
+++ SparkScalaCourse/src/com/jeitheronin/spark/FriendsByName.scala
@@ -8,8 +8,21 @@ object FriendsByName {
   
   def parseLine(line: String) = {
     val fields = line.split(",")
-    val name = fields(2).toString
+    val name = fields(0).toString
     val numFriends = fields(3).toInt
     (name, numFriends)
+  }
+  
+  // Main Function
+  def main(args: Array[String]){
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    
+    // Create a SparkContext using every core of the local machine    
+    val sc = new SparkContext("Local[*]", "FriendsByName")
+    
+    // Load each line of the source data into an RDD
+    val lines = sc.textFile("../fakefriends.csv")
+    
+    // Use our parseLines function to convert to ()
   }
 }
